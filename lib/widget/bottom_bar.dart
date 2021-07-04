9ee5cc4e3e79a4ed9c951e6124b83c48826f44e8
arimmie <arimy981002@likelion.org>
2021-07-05 00:36:26 +0900

    [add] bottom navigation bar

---
+++ lib/widget/bottom_bar.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class Bottom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //container안에 만들고자하는 BottomNavigationBAR 구성
+    return Container(
+      color: Colors.black,
+      child: Container(
+        height: 50,
+        child: TabBar(
+          labelColor: Colors.white,
+          unselectedLabelColor: Colors.white60,
+          indicatorColor: Colors.transparent,
+          tabs: <Widget>[
+            // # tab 1 (홈)
+            Tab(
+              icon: Icon(
+                Icons.home,
+                size: 18,
+              ),
+              child: Text(
+                '홈',
+                style: TextStyle(fontSize: 9),
+              ),
+            ),
+
+            // # tab 2 (검색)
+            Tab(
+              icon: Icon(
+                Icons.search,
+                size: 18,
+              ),
+              child: Text(
+                '검색',
+                style: TextStyle(fontSize: 9),
+              ),
+            ),
+
+            // # tab 3 (저장콘텐츠 목록)
+            Tab(
+              icon: Icon(
+                Icons.save_alt,
+                size: 18,
+              ),
+              child: Text(
+                '저장한 콘텐츠 목록',
+                style: TextStyle(fontSize: 9),
+              ),
+            ),
+
+            // # tab 4 (더보기)
+            Tab(
+              icon: Icon(
+                Icons.list,
+                size: 18,
+              ),
+              child: Text(
+                '더보기',
+                style: TextStyle(fontSize: 9),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
